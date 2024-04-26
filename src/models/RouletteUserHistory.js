612e93f5a913d9e73bb56f12bc0feb12fbc14f8f
ankit <iankitkhatrani@gmail.com>
2024-04-26 18:26:08 +0530

    add

--- src/models/RouletteUserHistory.js
+++ src/models/RouletteUserHistory.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const GameUser = mongoose.model("users");
+//const GameUser = mongoose.model("users");
 
 const collectionName = 'RouletteUserHistory';
 
 const RouletteUserSchema = new Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: GameUser },
+    //userId: { type: mongoose.Schema.Types.ObjectId, ref: GameUser },
     ballposition:{ type: Number},
     play:{ type: Number},
     won:{ type: Number},
