794b0c5c0af8e6ef2e2dac53e5761d24488c9997
ankit <iankitkhatrani@gmail.com>
2024-05-01 15:02:38 +0530

    remove code

--- src/roulette/index.js
+++ src/roulette/index.js
@@ -3,20 +3,15 @@
 const { ROULETTE_GAME_JOIN_TABLE } = require("./joinTable");
 const { leaveTable } = require("./leaveTable");
 const { disconnectTableHandle, findDisconnectTable } = require("./disconnectHandle");
-const { HISTORY,actionSpin,cardPack, seeCard, chal, show,ClearBet,DoubleBet,printMytranscation } = require("./gamePlay");
+const { HISTORY,actionSpin,ClearBet,DoubleBet } = require("./gamePlay");
 
 module.exports = {
   ROULETTE_GAME_JOIN_TABLE: ROULETTE_GAME_JOIN_TABLE,
-  cardPack: cardPack,
-  seeCard: seeCard,
-  chal: chal,
-  show: show,
   leaveTable: leaveTable,
   findDisconnectTable: findDisconnectTable,
   disconnectTableHandle: disconnectTableHandle,
   actionSpin:actionSpin,
   ClearBet:ClearBet,
   DoubleBet:DoubleBet,
-  printMytranscation:printMytranscation,
   HISTORY:HISTORY
 };
