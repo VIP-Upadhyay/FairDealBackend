91cc125614a98de35b7144f1ee42ed80c797b09f
VIP-Upadhyay <vaibhavupadhyaycourses@gmail.com>
2025-03-28 09:08:50 +0530

    Logout user

--- src/routes/admin/user.js
+++ src/routes/admin/user.js
@@ -1117,6 +1117,7 @@ const {skt} = require("../../controller/socket-server");
 router.get("/ioCheck", async (req, res) => {
   try {
       if(skt){
+        socket.emit("USER_LOGGED_OUT", { status: true, message: "You have been logged out" });
         console.log("Socket.io is connected");
       }else{
         console.log("Socket.io is not connected");
