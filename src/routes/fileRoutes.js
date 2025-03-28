4fe60a17d5fdc7bb70327adb2eff8c0376cf968f
VIP-Upadhyay <vaibhavupadhyaycourses@gmail.com>
2025-03-28 15:13:31 +0530

    Upload

--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -15,7 +15,7 @@ if (!fs.existsSync(uploadDir)) {
 const storage = multer.diskStorage({
   destination: uploadDir,
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+    cb(null, file.originalname);
   },
 });
 const upload = multer({ storage });
