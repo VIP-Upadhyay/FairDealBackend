48586ca4e3246293898fcaf5f81b29f95e18ea80
VIP-Upadhyay <vaibhavupadhyaycourses@gmail.com>
2025-04-02 15:41:14 +0530

    REJOIN USERS CHECKS

--- src/roulette/joinTable.js
+++ src/roulette/joinTable.js
@@ -330,3 +330,13 @@ module.exports.findEmptySeatAndUserSeat = async (table, client, requestData) => 
     
 //     return '-1'; // No empty seats available
 // };
+
+module.exports.findEmptySeat = (playerInfo) => {
+    for (x in playerInfo) {
+        if (typeof playerInfo[x] == 'object' && playerInfo[x] != null && typeof playerInfo[x].seatIndex == 'undefined') {
+            return parseInt(x);
+            break;
+        }
+    }
+    return '-1';
+}
